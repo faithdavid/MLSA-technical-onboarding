65922d50125c45279e710cbb2c58183c6c1f1bf8
faithdavid <59588415+faithdavid@users.noreply.github.com>
2023-11-01 22:46:16 +0000

    commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Adeboye Aregbesola",
+  name: "Adeboye Aregbesola Faith",
   title: "Power BI Analyst / Nodejs Developer",
   email: "afd.faithdavid@gmail.com",
   gitHub: "github.com/faithdavid",
